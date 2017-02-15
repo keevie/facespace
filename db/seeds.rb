cca03f60f27ee282d93dcc7a28d31d9315cbde01
Daniel Dyssegaard Kallick <ddyssegaardk@gmail.com>
2017-02-15 15:19:14 -0500

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create({email: 'guest@guest.com',
-             f_name: 'Guesty',
-             l_name: 'Guest',
-             dob: '1905-1-1',
-             password: 'password'
-})
