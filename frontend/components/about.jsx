0177481fc861ce43fa68b4fef12594ca721a1d42
Daniel Dyssegaard Kallick <ddyssegaardk@gmail.com>
2017-03-08 17:44:11 -0500

    style about

--- frontend/components/about.jsx
+++ frontend/components/about.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import moment from 'moment';
 
 const About = ({dob, location}) => {
   return (
     <section className="about">
+      <div id='about-title'>
+        <i className="fa fa-globe" aria-hidden="true"></i>
+        <h2>Intro</h2>
+      </div>
       <ul>
-        <li><p>Birthday: {dob}</p></li>
-        <li><p>location: {location}</p></li>
+        <li><p>
+          <i className="fa fa-birthday-cake" aria-hidden="true"></i>
+          Birthday: {moment(dob).format('MMMM D, YYYY')}
+        </p></li>
+        <li><p>
+          <i className="fa fa-map-marker" aria-hidden="true"></i>
+          Location: {location}
+        </p></li>
       </ul>
     </section>
   );
-}
+};
 
 export default About;
